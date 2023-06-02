06fbad9760afed9649933827ed83bd1a7cc44e33
papagaye744 <132003569+papagaye744@users.noreply.github.com>
2023-06-02 21:09:00 +0800

    update live go

--- api/live.go
+++ api/live.go
@@ -2,7 +2,7 @@
 package api
 
 import (
-  "/liveurls"
+  "../liveurls"
   "encoding/json"
 	"fmt"
   "net/http"
@@ -54,51 +54,52 @@ func duanyan(adurl string, realurl any) string {
 
 // vercel 平台会将请求传递给该函数，这个函数名随意，但函数参数必须按照该规则。
 func Live(w http.ResponseWriter, r *http.Request)  {
-    path := r.URL.Path
-    params := strings.Split(path, "/")
-    if len(params) >= 3 {
-      // 解析成功
-      // 直播平台
-      platform := params[1]
-      // 房间号
-      rid := params[2]
-      switch platform {
-      case "douyin":
-        douyinobj := &liveurls.Douyin{}
-        douyinobj.Rid = rid
-        http.Redirect(w, r, duanyan(adurl, douyinobj.GetDouYinUrl()), http.StatusMovedPermanently)
-      case "douyu":
-        douyuobj := &liveurls.Douyu{}
-        douyuobj.Rid = rid
-        douyuobj.Stream_type = defaultQuery(r, "stream", "hls")
-        douyuobj.Cdn_type = defaultQuery(r, "cdn", "akm-tct")
-        http.Redirect(w, r, duanyan(adurl, douyuobj.GetRealUrl()), http.StatusMovedPermanently)
-      case "huya":
-        huyaobj := &liveurls.Huya{}
-        huyaobj.Rid = rid
-        huyaobj.Cdn = defaultQuery(r, "cdn", "hwcdn")
-        huyaobj.Media = defaultQuery(r, "media", "flv")
-        huyaobj.Type = defaultQuery(r, "type", "nodisplay")
-        if huyaobj.Type == "display" {
-          returnJson(w, 200, huyaobj.GetLiveUrl())
-        } else {
-          http.Redirect(w, r, duanyan(adurl, huyaobj.GetLiveUrl()), http.StatusMovedPermanently)
-        }
-      case "bilibili":
-        biliobj := &liveurls.BiliBili{}
-        biliobj.Rid = rid
-        biliobj.Platform = defaultQuery(r, "platform", "web")
-        biliobj.Quality = defaultQuery(r, "quality", "10000")
-        biliobj.Line = defaultQuery(r, "line", "second")
-        http.Redirect(w, r, duanyan(adurl, biliobj.GetPlayUrl()), http.StatusMovedPermanently)
-      case "youtube":
-        ytbObj := &liveurls.Youtube{}
-        ytbObj.Rid = rid
-        ytbObj.Quality = defaultQuery(r, "quality", "1080")
-        http.Redirect(w, r, duanyan(adurl, ytbObj.GetLiveUrl()), http.StatusMovedPermanently)
+  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+  path := r.URL.Path
+  params := strings.Split(path, "/")
+  if len(params) >= 3 {
+    // 解析成功
+    // 直播平台
+    platform := params[1]
+    // 房间号
+    rid := params[2]
+    switch platform {
+    case "douyin":
+      douyinobj := &liveurls.Douyin{}
+      douyinobj.Rid = rid
+      http.Redirect(w, r, duanyan(adurl, douyinobj.GetDouYinUrl()), http.StatusMovedPermanently)
+    case "douyu":
+      douyuobj := &liveurls.Douyu{}
+      douyuobj.Rid = rid
+      douyuobj.Stream_type = defaultQuery(r, "stream", "hls")
+      douyuobj.Cdn_type = defaultQuery(r, "cdn", "akm-tct")
+      http.Redirect(w, r, duanyan(adurl, douyuobj.GetRealUrl()), http.StatusMovedPermanently)
+    case "huya":
+      huyaobj := &liveurls.Huya{}
+      huyaobj.Rid = rid
+      huyaobj.Cdn = defaultQuery(r, "cdn", "hwcdn")
+      huyaobj.Media = defaultQuery(r, "media", "flv")
+      huyaobj.Type = defaultQuery(r, "type", "nodisplay")
+      if huyaobj.Type == "display" {
+        returnJson(w, 200, huyaobj.GetLiveUrl())
+      } else {
+        http.Redirect(w, r, duanyan(adurl, huyaobj.GetLiveUrl()), http.StatusMovedPermanently)
       }
-    } else {
-      log.Println("Invalid path:", path)
-      returnJson(w, 500, path)
+    case "bilibili":
+      biliobj := &liveurls.BiliBili{}
+      biliobj.Rid = rid
+      biliobj.Platform = defaultQuery(r, "platform", "web")
+      biliobj.Quality = defaultQuery(r, "quality", "10000")
+      biliobj.Line = defaultQuery(r, "line", "second")
+      http.Redirect(w, r, duanyan(adurl, biliobj.GetPlayUrl()), http.StatusMovedPermanently)
+    case "youtube":
+      ytbObj := &liveurls.Youtube{}
+      ytbObj.Rid = rid
+      ytbObj.Quality = defaultQuery(r, "quality", "1080")
+      http.Redirect(w, r, duanyan(adurl, ytbObj.GetLiveUrl()), http.StatusMovedPermanently)
     }
+  } else {
+    log.Println("Invalid path:", path)
+    returnJson(w, 500, path)
+  }
 }
