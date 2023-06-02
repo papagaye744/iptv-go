0407372e0ead833456be8f26a2876dc1f4b80e32
papagaye744 <132003569+papagaye744@users.noreply.github.com>
2023-06-02 15:14:45 +0800

    Update live.go

--- api/live.go
+++ api/live.go
@@ -1,1 +1,13 @@
+// Package api
+package api
+import (
+ "liveurls"
+  "encoding/json"
+	"fmt"
+  "net/http"
+)
 
+// vercel 平台会将后面的请求传递给该 Tool 函数，这个函数名也随意，但函数参数必须按照该规则。
+func Tool(w http.ResponseWriter, r *http.Request)  {
+    // todo
+}
