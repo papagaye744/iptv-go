6a7c6c42e8a24faae3f0cd61fe9188f6dd3ec923
papagaye744 <132003569+papagaye744@users.noreply.github.com>
2023-06-08 16:40:44 +0800

    redirect test

--- api/test.go
+++ api/test.go
@@ -6,7 +6,7 @@ import (
 )
  
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
-//   http.Redirect(w, r, fmt.Sprintf("https://www.baidu.com?from=%s", "vercel"), http.StatusMovedPermanently) 
+  // fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+  http.Redirect(w, r, fmt.Sprintf("https://www.baidu.com?from=%s", "vercel"), http.StatusMovedPermanently) 
   return
 }
