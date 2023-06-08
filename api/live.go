6f3dcf7b1dde966c8669cb19941ef417eb8e3a82
papagaye744 <132003569+papagaye744@users.noreply.github.com>
2023-06-08 14:42:28 +0800

    update live

--- api/live.go
+++ api/live.go
@@ -8,27 +8,6 @@ import (
   "log"
 )
 
-type Response struct {
-  Code int `json:"code"`
-  Message string `json:"message"`
-}
-
-// 返回json数据
-func returnJson(w http.ResponseWriter, code int, message string) {
-  resp := Response {
-    Code: code,
-    Message: message
-  }
-  json, err := json.Marshal(resp)
-  if err != nil {
-    log.Println("Failed to encode JSON:", err)
-    http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-    return
-  }
-  w.Header().Set("Content-Type", "application/json")
-  w.Write(json)
-}
-
 // vercel 平台会将请求传递给该函数，这个函数名随意，但函数参数必须按照该规则。
 func Handler(w http.ResponseWriter, r *http.Request)  {
   path := r.URL.Path
@@ -36,9 +15,9 @@ func Handler(w http.ResponseWriter, r *http.Request)  {
   if len(params) >= 3 {
     platform := params[1]
     rid := params[2]
-    returnJson(w, 200, fmt.Sprintf("platform=%s, room=%s", platform, rid))
+    fmt.Sprintf("platform=%s, room=%s", platform, rid)
   } else {
     log.Println("Invalid path:", path)
-    returnJson(w, 500, path)
+    fmt.Sprintf("Invalid path: %s", path)
   }
 }
