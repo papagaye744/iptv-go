07c95831b24e2d933b98fd2cf28931cc311c8cbe
天仙婆婆 <tqx5201@163.com>
2024-07-31 19:08:41 +0800

    Update index.go

--- api/index.go
+++ api/index.go
@@ -17,7 +17,7 @@ import (
 
 // vercel 平台会将请求传递给该函数，这个函数名随意，但函数参数必须按照该规则。
 func Handler(w http.ResponseWriter, k *http.Request) {
-	enableTV = true
+	enableTV := true
  	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
